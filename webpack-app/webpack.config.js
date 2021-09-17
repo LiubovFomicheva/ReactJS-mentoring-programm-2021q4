d93d391c2694e0d0d44397edc3d9944655b37cfd
liubov_fomicheva <Liubov_Fomicheva@epam.com>
2021-09-17 16:49:32 +0300

    #task2: complete task with webpack for different envs

--- webpack-app/webpack.config.js
+++ webpack-app/webpack.config.js
@@ -1,27 +1,3 @@
-const path = require('path');
-const HtmlWebpackPlagin = require('html-webpack-plugin');
-
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ["*", ".js", ".jsx"],
-    },
-    plugins: [
-        new HtmlWebpackPlagin({
-            template: './src/index.html'
-        })
-    ]
-}
+module.exports = process.env.NODE_ENV === 'development'
+    ? require('./webpack.config.dev')
+    : require('./webpack.config.prod');
